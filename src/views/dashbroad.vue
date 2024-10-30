b891446d0580f4b8a7bf6f7de0d3c149241ad8cb
tianlin <li.tian.mc@qq.com>
2024-10-30 16:18:34 +0800

    update

--- src/views/dashbroad.vue
+++ src/views/dashbroad.vue
@@ -4,9 +4,10 @@
     *，ts的使用，基础用法
     *，Webpack5：常见配置，微前端的使用
     *，Vite+ESBuild
-    *，vue的响应式原理深入，模拟API的使用
     *，服务端渲染-SSR和SSG，Vite.js，Nuxt.js
     *，Node.js中间件的使用，服务器端编程
+    JS Worker的使用
+    indexedDB的使用
 
     *，数据结构和算法
     *，leetcode算法练习
